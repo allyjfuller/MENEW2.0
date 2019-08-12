763cb8c26524eff309d76685462081932b95aea5
allyjfuller <allyjfuller@gmail.com>
2019-08-12 18:22:41 -0400

    header logo

---
+++ menew2.0_client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+export function Header() {
+	return(
+
+		<div className="logo">
+			<h1>Menew</h1>
+		</div>
+
+	);
+}
+
+export default Header
